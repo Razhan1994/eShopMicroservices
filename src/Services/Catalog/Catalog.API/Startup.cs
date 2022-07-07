147e462831d4935293fae48ea4b2387780cde0cf
javidkolurazhan <javidklurajan@gmail.com>
2022-07-08 01:23:54 +0430

    add startup class

---
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -0,0 +1,38 @@
+﻿namespace Catalog.API
+{
+    public class Startup
+    {
+        public IConfiguration _configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureService(IServiceCollection services)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+        }
+
+        public void configure(WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
